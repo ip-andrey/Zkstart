caf339241e9899337b8b00c0c048fd603008c164
ip-andrey <101020589+ip-andrey@users.noreply.github.com>
2024-02-23 15:56:29 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,28 @@
+# RANDOM WALLETS MODE
+RANDOM_WALLET = True  # True or False
+
+# removing a wallet from the list after the job is done
+REMOVE_WALLET = False
+
+SLEEP_FROM = 100  # Second
+SLEEP_TO = 300  # Second
+
+QUANTITY_THREADS = 4
+
+THREAD_SLEEP_FROM = 50
+THREAD_SLEEP_TO = 150
+
+# PROXY MODE
+USE_PROXY = True
+
+# GWEI CONTROL MODE
+CHECK_GWEI = True  # True or False
+MAX_GWEI = 25
+
+GAS_MULTIPLIER = 0.8
+
+# RETRY MODE
+RETRY_COUNT = 3
+
+# INCH API KEY
+INCH_API_KEY = ""
